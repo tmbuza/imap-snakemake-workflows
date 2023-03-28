33c1ec60dc30e09e80f2beaf453161f34bf1de47
tmbuza <ndelly@gmail.com>
2023-03-28 09:00:37 -0500

    Update workflows

--- workflow/scripts/files4smk_report.sh
+++ workflow/scripts/files4smk_report.sh
@@ -5,8 +5,8 @@ cp ../imap-sample-metadata/dags/rulegraph.svg images/imap_part02.svg
 cp ../imap-download-sra-reads/dags/rulegraph.svg images/imap_part03.svg
 cp ../imap-read-quality-control/dags/rulegraph.svg images/imap_part04.svg
 cp ../imap-bioinformatics-mothur/dags/rulegraph.svg images/imap_part05.svg
-cp ../imap-bioinformatics-mothur/dags/rulegraph.svg images/imap_part06.svg
-cp ../imap-bioinformatics-mothur/dags/rulegraph.svg images/imap_part07.svg
+cp ../imap-bioinformatics-qiime2/dags/rulegraph.svg images/imap_part06.svg
+cp ../imap-metagenomics-viral/dags/rulegraph.svg images/imap_part07.svg
 cp ../global/imap.png images/imap.png
 
 
